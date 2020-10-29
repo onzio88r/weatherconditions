54bcd8cb06c014afc0bf10385cbc7181949cf5bc
Daniele Rapali <daniele.rapali@gmail.com>
2020-10-29 17:19:21 +0100

    textfield for search city and little animation ( to review )

--- weatherconditions/weatherconditions/Controllers/MainViewController.swift
+++ weatherconditions/weatherconditions/Controllers/MainViewController.swift
@@ -11,6 +11,11 @@ import Combine
 class MainViewController: UIViewController {
     @IBOutlet weak var backgroundImage: UIImageView!
     
+    @IBOutlet weak var searchButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textFieldLeadingConstraint: NSLayoutConstraint!
+    
+    
     private var disposables = Set<AnyCancellable>()
     
     
@@ -23,9 +28,25 @@ class MainViewController: UIViewController {
                   receiveValue: { print($0) })
             .store(in: &disposables)
         
-
+        textFieldLeadingConstraint.constant = searchButton.frame.origin.x - 10
+        self.textField.alpha = 0
     }
-
+    
+    
+    @IBAction func searchAction(_ sender: Any) {
+        
+        //TODO: little animation for text field, review and move in specifi function
+        textFieldLeadingConstraint.constant = 20
+        self.textField.alpha = 1
+        UIView.animateKeyframes(withDuration: 0.5, delay: 0, options: .autoreverse, animations: {
+            
+            self.textField.layoutIfNeeded()
+        }, completion: nil)
+        
+        
+    
+    }
+    
 
 }
 
