5d43652520fdb97d42b71ce0fe66f85a473e242a
Daniele Rapali <daniele.rapali@gmail.com>
2020-11-02 10:41:40 +0100

    Api adjustment for query parameters

--- weatherconditions/weatherconditions/API Service/APIService.swift
+++ weatherconditions/weatherconditions/API Service/APIService.swift
@@ -10,36 +10,49 @@ import Combine
 
 enum WeatherAPI {
     static let agent = APIAgent()
+    
+    static func urlComponentFrom(city: String) -> URLComponents{
+        var component = URLComponents()
+        component.scheme = "http"
+        component.path = forecastAPIPath
+        component.host = openweatherURL
+        component.queryItems = [
+            
+            URLQueryItem(name: "q", value: city),
+            URLQueryItem(name: "units", value: "metric"),
+            URLQueryItem(name: "appid", value: "e53a70fab51d98c3061bff74b4c4b010")]
+        
+        return component
+    }
+    
 }
 
 extension WeatherAPI {
     
     static func forecast(city: String) -> AnyPublisher<Forecast, Error> {
-        let stringCity = city.replacingOccurrences(of: " ", with: "")
-        let baseUrl = URL(string: String(format: openweatherURL, stringCity))!
-        let request = URLRequest(url: baseUrl) //TODO: Add parameters for selected city
-           return run(request)
-       }
+        let urlComponent = WeatherAPI.urlComponentFrom(city: city)
+        let request = URLRequest(url: urlComponent.url!)
+        return run(request)
+    }
     
     static func weatherIcon(icon: String) -> AnyPublisher<Data, Error> {
         let baseUrlIcon = URL(string: String(format: weatherIConURL, icon))!
         
         let request = URLRequest(url: baseUrlIcon)
-           return run(request)
-       }
+        return run(request)
+    }
     
     static func run<T: Decodable>(_ request: URLRequest) -> AnyPublisher<T, Error> {
-            return agent.run(request)
-                .map(\.value)
-                .eraseToAnyPublisher()
-        }
+        return agent.run(request)
+            .map(\.value)
+            .eraseToAnyPublisher()
+    }
     
     static func run(_ request: URLRequest) -> AnyPublisher<Data, Error> {
-            return agent.run(request)
-                .map(\.value)
-                .eraseToAnyPublisher()
-        }
+        return agent.run(request)
+            .map(\.value)
+            .eraseToAnyPublisher()
+    }
     
     
 }
-
---
+++ weatherconditions/weatherconditions/ViewModels/ForecastCellViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  ForecastTableViewModel.swift
+//  weatherconditions
+//
+//  Created by Daniele Rapali on 30/10/2020.
+//
+
+import Foundation
+import Combine
+
+class ForecastCellViewModel: ObservableObject {
+    private var disposables = Set<AnyCancellable>()
+    private (set) var weatherIconData: Data!
+
+    
+    func fetchIcon(_ icon:String){
+        WeatherAPI.weatherIcon(icon: icon)
+            .sink(receiveCompletion: {_ in },
+                  receiveValue: {
+                    self.weatherIconData = $0
+                    self.objectWillChange.send()
+                  })
+            .store(in: &disposables)
+        
+        
+        
+    }
+    
+}
